6460711c2b0a8bbc71bb30ebb22ba300dd6acfa6
Nayade Aguilar <nayadeaguilar@Nayades-MacBook-Air.local>
2025-09-18 12:14:58 -0700

    Created code to make James bond activity

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        /*
+        What is your first name? Scott
+        What is your last name? Burton
+
+        Your name is Burton, Scott Burton.
+        */
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
