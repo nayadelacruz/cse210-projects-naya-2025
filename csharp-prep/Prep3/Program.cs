498365783e4c753198c1db6dbf1e4c9bd4dfeb7b
Nayade Aguilar <nayadeaguilar@Nayades-MacBook-Air.local>
2025-09-20 14:51:57 -0700

    created a program that generates a random number and user has to guess it

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        /* Console.Write("WHat is the magic number? ");
+         string answer = Console.ReadLine();
+         int magicNumber = int.Parse(answer);*/
+
+        Random random = new Random();
+        int magicNumber = random.Next(1, 100);
+
+        Console.Write(" Between 1-100, what is your guess number? ");
+        string guessString = Console.ReadLine();
+        int guess = int.Parse(guessString);
+        
+        while (guess != magicNumber)
+        {
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("go higher");
+            }
+            else
+            {
+                Console.WriteLine("go lower");
+            }
+
+            Console.Write("What is your guess? ");
+            guessString = Console.ReadLine();
+            guess = int.Parse(guessString);
+        }
+        if (guess == magicNumber)
+        {
+            Console.WriteLine("You guess it!");
+        }
+     
+
     }
 }
