06dcd3f98c3a247c17cd2dbdacbff321b2a92f3a
Nayade Aguilar <nayadeaguilar@Nayades-MacBook-Air.local>
2025-09-20 16:24:43 -0700

    created a code were the user creates a list and then it shows the sum, average and bigger number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+
         Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter a number: ");
+        string entry = Console.ReadLine();
+        int number = int.Parse(entry);
+        List<int> numbers = new List<int>();
+        while (number != 0)
+        {
+            numbers.Add(number);
+            Console.Write("Enter a number: ");
+            entry = Console.ReadLine();
+            number = int.Parse(entry);
+
+        }
+        /* foreach (int numero in numbers)
+         {
+             Console.Write(numero);
+         }*/
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+        int average = sum / numbers.Count;
+        Console.WriteLine($"The average of the numbers is: {average}");
+
+        int bigger = numbers[0];
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] > bigger)
+            {
+                bigger = numbers[i];
+            }
+        }
+        Console.WriteLine($"The biggest number is: {bigger}");
+        /*
+        */
     }
 }
