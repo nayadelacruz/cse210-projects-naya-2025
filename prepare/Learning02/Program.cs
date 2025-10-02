0b0a67ba6840b6aaaa9e6f727ba7b696fcd564ce
Nayade Aguilar <nayadeaguilar@Nayades-MacBook-Air.local>
2025-10-01 17:33:41 -0700

    created a code that shows a resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Quality Assurance Enginer";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Software Developer";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        /*job1.Display();
+        job2.Display();
+        Console.WriteLine(job1._company);*/
+
+        Resume myResume = new Resume();
+        myResume._name = "Nayade De la cruz";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        /*Console.WriteLine(myResume._jobs[0]._jobTitle);*/
+        myResume.Display();
     }
 }
