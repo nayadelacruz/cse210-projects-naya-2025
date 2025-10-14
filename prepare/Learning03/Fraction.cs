9fdc702b58374d443ea1d86821d1812f7e40ea4e
Nayade Aguilar <nayadeaguilar@Nayades-MacBook-Air.local>
2025-10-14 11:37:07 -0700

    created program that returns fractions and decimals

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,53 @@
+using System;
+
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+
+    }
+
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+    public int GetDenominator()
+    {
+        return _denominator;
+    }
+    public void SetNumerator(int numerator)
+    {
+        _numerator = numerator;
+    }
+
+    public void SetDenominator(int denominator)
+    {
+        _denominator = denominator;
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;
+    }       
+    public string FractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }   
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Test all constructors
+        Fraction f1 = new Fraction();        // 1/1
+        Fraction f2 = new Fraction(6);       // 6/1
+        Fraction f3 = new Fraction(3, 4);    // 3/4
+
+        // Display each fraction and its decimal value
+        Console.WriteLine(f1.FractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.FractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.FractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        // Test getters and setters
+        f3.SetNumerator(1);
+        f3.SetDenominator(3);
+        Console.WriteLine($"Updated fraction: {f3.FractionString()}  = {f3.GetDecimalValue()}");
     }
 }
